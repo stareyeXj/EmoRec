0717a966e55207b8f835b084859122f098f05dfc
stareyeXj <133827494+stareyeXj@users.noreply.github.com>
2025-06-20 14:27:12 +0800

    Add files via upload

---
+++ SECap框架/inference.py
@@ -0,0 +1,44 @@
+import sys
+
+sys.path.append("..")
+
+# from dataloader.dataloader import AudioMotionDataset, collate_fn
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset, DataLoader, RandomSampler
+from transformers import AutoTokenizer
+from model2 import MotionAudio
+from torch.nn import CrossEntropyLoss
+import torch
+from lightning.pytorch import Trainer, LightningDataModule, LightningModule, Callback, seed_everything
+from lightning.pytorch.callbacks import ModelCheckpoint
+from lightning.pytorch.loggers import TensorBoardLogger
+import lightning.pytorch as pl
+import torch.optim as optim
+import math
+import argparse
+import soundfile as sf
+import torchaudio
+import os
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    path = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.dirname(path)
+    path = os.path.join(path, 'dataset/wav/tx_emotion_00201000015.wav')
+    # you can change the wavdir to your own wav file
+    parser.add_argument("--wavdir", type=str, default=path)
+    model = MotionAudio()
+
+    wavdir = parser.parse_args().wavdir
+
+    wav, sr = sf.read(wavdir)
+    if sr != 16000:
+        waveform = torchaudio.transforms.Resample(sr, 16000)(torch.tensor(wav).unsqueeze(0).to(torch.float32)).squeeze(
+            0).numpy()  # print(sample_rate)
+    wavform = [waveform]
+
+    torch.cuda.empty_cache()
+    state_dict = torch.load(r"C:\Users\lenovo\Desktop\model.ckpt", map_location=torch.device('cpu'),weights_only=True)
+    model.load_state_dict(state_dict,strict=False)
+    model = model.to(torch.device('cpu'))
+    model.inference(wavform)
---
+++ SECap框架/test.py
@@ -0,0 +1,42 @@
+import os
+import sys
+sys.path.append("..")
+
+from dataloader.dataloader import AudioMotionDataset, collate_fn
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset, DataLoader, RandomSampler
+from transformers import AutoTokenizer
+from model2 import MotionAudio
+from torch.nn import CrossEntropyLoss
+import torch
+from lightning.pytorch import Trainer, LightningDataModule, LightningModule, Callback, seed_everything
+from lightning.pytorch.callbacks import ModelCheckpoint
+from lightning.pytorch.loggers import TensorBoardLogger
+import lightning.pytorch as pl
+import torch.optim as optim
+import math
+
+
+pl.seed_everything(666)
+model=MotionAudio()
+    
+batch_size=1
+
+AM_Dataset = AudioMotionDataset("../dataset/text.txt","../dataset/wav.scp","../dataset/fid2captions.json")
+AM_Dataloader = DataLoader(AM_Dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn,prefetch_factor=2,persistent_workers=True,num_workers=8)
+
+torch.cuda.empty_cache()
+state_dict = torch.load("../model.ckpt",map_location=torch.device('cpu'))
+model.load_state_dict(state_dict)
+torch.cuda.empty_cache()
+#In this case, we use 8 GPUs to test the model
+#It will take about 10 minutes to test the model on 8 V100 GPUs
+
+trainer = pl.Trainer(
+    accelerator='gpu',
+    devices=8,
+    precision="32"
+    )
+
+
+trainer.test(model,AM_Dataloader)
